40a01bb8cf8d7344ebc7ca19523e7ba5e27530c1
Burak Yıldız <cang34gs@gmail.com>
2022-08-03 15:04:21 +0300

    .net unit test example added
    .net unit test example added

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using webdenemeunit.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AboutHakkinda()
+        {
+            //arrange
+            HomeController controller = new HomeController();
+            //act
+            ViewResult sonuc = controller.About() as ViewResult;
+            //assert
+            Assert.AreEqual("bizim mesajımız", sonuc.ViewBag.Message);
+        }
+    }
+}
